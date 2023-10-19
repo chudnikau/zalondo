6951fa10be2ad7d43fa51edbfc1c8cb72751b2f0
chudnikau <chudnikau@gmail.com>
2023-10-19 14:24:48 +0300

    Add solution

--- tasks/longest-string/src/com/chudnikau/longeststring/Main.java
+++ tasks/longest-string/src/com/chudnikau/longeststring/Main.java
@@ -1,10 +1,15 @@
 package com.chudnikau.longeststring;
 
-import com.chudnikau.leadingzeros.Solution;
-
 public class Main {
     public static void main(String[] args) {
-        com.chudnikau.leadingzeros.Solution solution = new Solution();
-        System.out.println(solution.solution("Main"));
+        Solution solution = new Solution();
+        asserEquals(solution.solution(5, 0, 2).equals("AABBAABBAA"));
+        asserEquals(solution.solution(1, 2, 1).equals("AABBABAB"));
+        asserEquals(solution.solution(0, 2, 0).equals("ABAB"));
+        asserEquals(solution.solution(0, 0, 10).equals("BB"));
+    }
+
+    static void asserEquals(boolean b) {
+        if (!b) throw new RuntimeException();
     }
 }
--- tasks/longest-string/src/com/chudnikau/longeststring/Solution.java
+++ tasks/longest-string/src/com/chudnikau/longeststring/Solution.java
@@ -1,8 +1,68 @@
 package com.chudnikau.longeststring;
 
+import java.util.Comparator;
+import java.util.Objects;
+import java.util.stream.Stream;
+
 public class Solution {
 
-    public String solution(String s) {
-        return s;
+    public String solution(int AA, int AB, int BB) {
+        // Implement your solution here
+        if (AA < 0 || AA > 10) throw new RuntimeException();
+        if (AB < 0 || AB > 10) throw new RuntimeException();
+        if (BB < 0 || BB > 10) throw new RuntimeException();
+
+        String s1 = findSolution("AA", AA, AB, BB);
+        String s2 = findSolution("AB", AA, AB, BB);
+        String s3 = findSolution("BB", AA, AB, BB);
+
+        String resStr = Stream.of(s1, s2, s3)
+                .max(Comparator.comparingInt(String::length))
+                .orElse(null);
+
+        if (Objects.isNull(resStr)) throw new RuntimeException();
+
+        return resStr;
+    }
+
+    private String findSolution(String initStr, int AA, int AB, int BB) {
+        String S = "";
+        String lastStr = initStr;
+        while (true) {
+            String instStr = "";
+            switch (lastStr) {
+                case "AA":
+                    if (BB > 0) {
+                        instStr = "BB";
+                        BB--;
+                    }
+                    break;
+                case "AB":
+                    if (AA > 0) {
+                        instStr = "AA";
+                        AA--;
+                    }
+                    if (AB > 0) {
+                        instStr = "AB";
+                        AB--;
+                    }
+                    break;
+                case "BB":
+                    if (AA > 0) {
+                        instStr = "AA";
+                        AA--;
+                    } else {
+                        if (AB > 0) {
+                            instStr = "AB";
+                            AB--;
+                        }
+                    }
+                    break;
+            }
+            if (instStr.isEmpty()) break;
+            S = S + instStr;
+            lastStr = instStr;
+        }
+        return S;
     }
 }
